183df0d5b1540c9efb68c947684d1c4e43736c6e
pavelshen <pavelshen@foxmail.com>
2018-02-24 17:51:54 +0800

    week 15

---
+++ src/0083_RemoveDuplicatesFromSortedList.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    if(head === null || head.next === null) return head
+    
+    if(head.val === head.next.val){
+      head.next = head.next.next
+      deleteDuplicates(head)
+    } else {
+      deleteDuplicates(head.next)
+    }
+    
+    return head
+    
+  };
